5bcb5e07ae6e01a2bbfc7aab62077a5539a97210
ruoxiang-wen <ruoxiang.wen@alteredstatemachine.xyz>
2024-08-03 01:08:08 +1000

    custom node name

--- __init__.py
+++ __init__.py
@@ -44,7 +44,7 @@ def jen_process_check(id):
                 return False, download_url
         else:
             return True, download_url
-def jen_get_api_key_from_config():
+def jen_setup():
     p = os.path.dirname(os.path.realpath(__file__))
     path_config = os.path.join(p, "config.json")
     f = open(path_config)
@@ -56,9 +56,13 @@ def jen_get_api_key_from_config():
     data = json.load(f1)
     jen_api_endpoint = data["JEN_API_ENDPOINT"]
 
-    return jen_api_key, jen_api_endpoint
+    path_node = os.path.join(p, "node.json")
+    f2 = open(path_node)
+    jen_node_mapping = json.load(f2)
 
-jen_api_key, jen_api_endpoint = jen_get_api_key_from_config()
+    return jen_api_key, jen_api_endpoint, jen_node_mapping
+
+jen_api_key, jen_api_endpoint, jen_node_mapping = jen_setup()
 
 class JEN_download:
     def __init__(self):
@@ -70,7 +74,6 @@ class JEN_download:
                 "id": ("STRING", {"default": "123-123-123"}),
                 "format": ("STRING", {"default": "mp3"}),
                 "dest_dir": ("STRING", {"default": "output/JEN"}),
-                "local_test": ("BOOLEAN", {"default": False},),
             }
         }
 
@@ -81,13 +84,13 @@ class JEN_download:
     OUTPUT_NODE = True
     CATEGORY = "JEN"
 
-    def run(self, id, format, dest_dir, local_test):
+    def run(self, id, format, dest_dir):
         assert format in ["wav", "mp3"]
         os.makedirs(os.path.join(dir_current, dest_dir), exist_ok=True)
         dest_dir = os.path.join(dir_current, dest_dir)
         path_generate = os.path.join(dest_dir, "download." + format)
         logging.info("path_generate {}".format(path_generate))
-        if not local_test:
+        if True:
             validated, download_url = jen_process_check(id)
             if validated:
                 data_genearte = requests.get(download_url).content
@@ -107,7 +110,6 @@ class JEN_generate:
                 "fadeOutLength": ("INT", {"default": 0}),
                 "duration": ("INT", {"default": 10}),
                 "dest_dir": ("STRING", {"default": "output/JEN"}),
-                "local_test": ("BOOLEAN", {"default": False},),
             }
         }
 
@@ -118,7 +120,7 @@ class JEN_generate:
     OUTPUT_NODE = True
     CATEGORY = "JEN"
 
-    def run(self, prompt, format, fadeOutLength, duration, dest_dir, local_test):
+    def run(self, prompt, format, fadeOutLength, duration, dest_dir):
         assert format in ["wav", "mp3"]
         assert duration in [10, 45]
         os.makedirs(os.path.join(dir_current, dest_dir), exist_ok=True)
@@ -126,7 +128,7 @@ class JEN_generate:
         path_generate = os.path.join(dest_dir, "generate." + format)
         logging.info("path_generate {}".format(path_generate))
         id = ""; creditBalance = "/"
-        if not local_test:
+        if True:
             url = "{}/api/v1/public/track/generate".format(jen_api_endpoint)
             payload = {'prompt': prompt, "format": format, "fadeOutLength": fadeOutLength, "duration": duration}
             # payload = json.dumps(payload)
@@ -176,7 +178,6 @@ class JEN_extend:
                 "fadeOutLength": ("INT", {"default": 0}),
                 "duration": ("INT", {"default": 20}),
                 "dest_dir": ("STRING", {"default": "output/JEN"}),
-                "local_test": ("BOOLEAN", {"default": False},),
             }
         }
 
@@ -187,7 +188,7 @@ class JEN_extend:
     OUTPUT_NODE = True
     CATEGORY = "JEN"
 
-    def run(self, id, prompt, format, fadeOutLength, duration, dest_dir, local_test):
+    def run(self, id, prompt, format, fadeOutLength, duration, dest_dir):
         assert format in ["wav", "mp3"]
         assert duration in [20, 40]
         os.makedirs(os.path.join(dir_current, dest_dir), exist_ok=True)
@@ -195,7 +196,7 @@ class JEN_extend:
         path_extend = os.path.join(dest_dir, "extend." + format)
         logging.info("path_extend {}".format(path_extend))
         creditBalance = "/"
-        if not local_test:
+        if True:
             url = "{}/api/v1/public/track/extend/{}".format(jen_api_endpoint, id)
             payload = {'prompt': prompt, "format": format, "fadeOutLength": fadeOutLength, "duration": duration}
             # payload = json.dumps(payload)
@@ -237,9 +238,9 @@ NODE_CLASS_MAPPINGS = {
     "JEN_extend": JEN_extend
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "JEN_generate": "ComfyUI-JEN-generate",
-    "JEN_download": "ComfyUI-JEN-download",
-    "JEN_extend": "ComfyUI-JEN-extend"
+    "JEN_generate": jen_node_mapping["JEN_generate"],
+    "JEN_download": jen_node_mapping["JEN_download"],
+    "JEN_extend": jen_node_mapping["JEN_extend"],
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
@@ -253,14 +254,14 @@ if __name__ == '__main__':
     # format = "mp3"
     # fadeOutLength = 0
     # duration = 10
-    # dest_dir = "."; local_test = False
-    # JEN_generate.run(prompt, format, fadeOutLength, duration, dest_dir, local_test)
+    # dest_dir = "."
+    # JEN_generate.run(prompt, format, fadeOutLength, duration, dest_dir)
 
     # id = "123123"
     # prompt = "party edm"
     # format = "mp3"
     # fadeOutLength = 0
     # duration = 20
-    # dest_dir = "."; local_test = False
-    # JEN_extend.run(id, prompt, format, fadeOutLength, duration, dest_dir, local_test)
+    # dest_dir = "."
+    # JEN_extend.run(id, prompt, format, fadeOutLength, duration, dest_dir)
 
